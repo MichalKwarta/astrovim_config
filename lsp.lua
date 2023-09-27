801aed22f0903120d422a61a4528e684804f4200
extern-kwarta-michal <michal@cloudprizm.io>
2023-09-27 14:08:32 +0200

    split config into multiple files

---
+++ lsp.lua
@@ -0,0 +1,9 @@
+return {
+  formatting = {
+    format_on_save = {
+      enabled = true, -- enable format on save
+      ignore_filetypes = { -- disable format on save for specified filetypes
+      },
+    },
+  },
+}
