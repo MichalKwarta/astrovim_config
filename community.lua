7fc471031ce8cd8821fd0ec98a1eab94fd3316ad
MichalKwarta <Michal.r.kwarta@gmail.com>
2024-08-24 19:21:07 +0200

    comment hardtime

--- community.lua
+++ community.lua
@@ -34,9 +34,9 @@ local astro_packs = {
   project = {
     "nvim-spectre",
   },
-  workflow = {
-    "hardtime-nvim",
-  },
+  -- workflow = {
+  --   "hardtime-nvim",
+  -- },
 }
 function assemble_packs(packs)
   local importTable = {}
